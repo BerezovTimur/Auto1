413697c5c507a043d7f0af7ec1cf4c6a16294ca1
Timur Berezov <Berezov.Timur@gmail.com>
2020-06-22 21:54:19 +0300

    Add testng branch

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,38 +1,37 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.assertEquals;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
     CashbackHackService cashbackHackService = new CashbackHackService();
 
     @Test
     void shouldBuyUnder1000() {
         int actual = cashbackHackService.remain(900);
         int expected = 100;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
     @Test
     void shouldBuyOver1000() {
         int actual = cashbackHackService.remain(1200);
         int expected = 800;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
     @Test
     void shouldBuyButNotBuy() {
         int actual = cashbackHackService.remain(0);
         int expected = 1000;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
     @Test
     void shouldBuy1000() {
         int actual = cashbackHackService.remain(1000);
         int expected = 0;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
-
 }
